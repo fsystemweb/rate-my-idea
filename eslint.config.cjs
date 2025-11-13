d589a2347ce95e0f96bbb73e63d56e4a4fd72ee7
fsystemweb <facu.cappella@gmail.com>
2025-11-13 17:52:14 +0100

    fix lint

--- eslint.config.cjs
+++
@@ -1,7 +0,0 @@
-
-const { FlatCompat } = require('@eslint/eslintrc');
-
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: {} });
-
-const legacy = require('./.eslintrc.cjs');
-module.exports = compat.config(legacy);
