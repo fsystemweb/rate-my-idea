21b3405cdb660d4b0e9b796571a40f8ccce4cf8d
fsystemweb <facu.cappella@gmail.com>
2025-11-28 18:10:06 +0100

    fix format

--- apps/client/main.tsx
+++ apps/client/main.tsx
@@ -4,9 +4,8 @@ import * as Sentry from "@sentry/react";
 
 const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
 
-
 Sentry.init({
   dsn: SENTRY_DSN,
-  sendDefaultPii: true
+  sendDefaultPii: true,
 });
 createRoot(document.getElementById("root")!).render(<App />);
